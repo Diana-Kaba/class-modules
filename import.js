1c56198c2ba1d1bb0df10adc41c9eeb1431d3636
Diana-Kaba <kabadiana327@gmail.com>
2023-04-04 09:31:15 +0300

    Add descendant classes

--- import.js
+++ import.js
@@ -1,3 +1,10 @@
-import Animal from "./script.js";
-let animal = new Animal("Dog", 10);
+import { Animal, Dog, Cat } from "./script.js";
+
+let animal = new Animal("Hamster", 2);
 console.log(animal.animalCharacteristic);
+
+let dog = new Dog("Foxter", 10);
+console.log(dog.animalCharacteristic);
+
+let cat = new Cat("Myrka", 5);
+console.log(cat.animalCharacteristic);
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export default class Animal {
+export class Animal {
   constructor(name, age) {
     this.name = name;
     this.age = age;
@@ -13,5 +13,25 @@ export default class Animal {
   set animalCharacteristic(newValue) {
     this.name = newValue;
     this.age = newValue;
+  }
+}
+
+export class Dog extends Animal {
+  constructor(name, age) {
+    super(name, age);
+  }
+  walk() {
+    super.walk();
+    console.log(`${this.name} barks!`);
+  }
+}
+
+export class Cat extends Animal {
+  constructor(name, age) {
+    super(name, age);
+  }
+  walk() {
+    super.walk();
+    console.log(`${this.name} meows!`);
   }
 }
